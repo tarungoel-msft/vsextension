b7da87794a0a68fcc593af220f77b0cb42fefd81
WHOIS: ldap_open failed <tarungoel@microsoft.com>
2023-09-13 13:16:57 +0530

    getting function body

--- Analyzer1/Analyzer1/Analyzer1Analyzer.cs
+++ Analyzer1/Analyzer1/Analyzer1Analyzer.cs
@@ -13,7 +13,7 @@ namespace Analyzer1
     [DiagnosticAnalyzer(LanguageNames.CSharp)]
     public class Analyzer1Analyzer : DiagnosticAnalyzer
     {
-        public const string DiagnosticId = "Analyzer1";
+        public const string DiagnosticId = "Duplicate function";
 
         // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
         // See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/Localizing%20Analyzers.md for more on localization
@@ -33,7 +33,16 @@ namespace Analyzer1
 
             // TODO: Consider registering other actions that act on syntax instead of or in addition to symbols
             // See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/Analyzer%20Actions%20Semantics.md for more information
-            context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.NamedType);
+            context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.Method);
+            context.RegisterCodeBlockAction(AnalyzeCodeBlock);
+        }
+
+        private static void AnalyzeCodeBlock(CodeBlockAnalysisContext context)
+        {
+            var methodSymbol = context.OwningSymbol as IMethodSymbol;
+            var methodBody = context.CodeBlock;
+            var diagnostic = Diagnostic.Create(Rule, methodSymbol.Locations[0], methodSymbol.Name);
+            context.ReportDiagnostic(diagnostic);
         }
 
         private static void AnalyzeSymbol(SymbolAnalysisContext context)
